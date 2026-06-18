ea7e8ef977963dfd703a49a12653b2715613c156
Gauhar <asanbayg@gmail.com>
2026-06-18 21:29:23 +0500

    add draw dot function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
-#include <iostream>
-
 #include "engine.hpp"
+#include "trace_graph.cpp"
 
 int main(int argc, char* argv[]) {
-  std::cout << "Work in progress";
+  std::cout << "Drawing dummy graph..." << std::endl;
 
+  auto x1 = Value::make(-1.0);
+  Value::make(-1.0);
+  auto x2 = Value::make(2.0);
+  auto w1 = Value::make(3.0);
+  auto w2 = Value::make(1.5);
+
+  auto b = Value::make(0.5);
+
+  // Forward Pass: x1*w1 + x2*w2 + b
+  auto x1w1 = Value::make(x1->data * w1->data, std::unordered_set{x1, w1}, "*");
+  auto x2w2 = Value::make(x2->data * w2->data, std::unordered_set{x2, w2}, "*");
+
+  auto sum =
+      Value::make(x1w1->data + x2w2->data, std::unordered_set{x1w1, x2w2}, "+");
+  auto logit =
+      Value::make(sum->data + b->data, std::unordered_set{sum, b}, "+");
+
+  // Activation function
+  auto out = Value::make(0.9999, std::unordered_set{logit}, "tanh");
+
+  // Backward Pass
+  out->grad = 1.0;
+  logit->grad = 0.5;
+  b->grad = 0.5;
+  sum->grad = 0.5;
+  x1w1->grad = 0.5;
+  x2w2->grad = 0.5;
+  x1->grad = 1.5;
+  w1->grad = -0.5;
+  x2->grad = 0.75;
+  w2->grad = 1.0;
+
+  // Draw the graph!
+  drawDot(out, "LR");
+
+  std::cout << "Finished!" << std::endl;
   return 0;
 }
---
+++ trace_graph.cpp
@@ -0,0 +1,67 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+#include "engine.hpp"
+
+void trace(ValuePtr root, std::unordered_set<ValuePtr>& nodes,
+           std::vector<std::pair<ValuePtr, ValuePtr>>& edges) {
+  if (nodes.find(root) == nodes.end()) {
+    nodes.insert(root);
+    for (const auto& child : root->_prev) {
+      edges.push_back({child, root});
+      trace(child, nodes, edges);
+    }
+  }
+}
+
+void drawDot(ValuePtr root, std::string rankdir = "LR") {
+  std::unordered_set<ValuePtr> nodes;
+  std::vector<std::pair<ValuePtr, ValuePtr>> edges;
+
+  trace(root, nodes, edges);
+
+  std::ofstream dotFile("graph.dot");
+  if (!dotFile.is_open()) {
+    std::cerr << "Failed to open dot file for writing" << std::endl;
+    return;
+  }
+
+  dotFile << "digraph G{\n";
+  dotFile << " rankdir=" << rankdir << ";\n";
+
+  for (const auto& n : nodes) {
+    std::stringstream ss_id;
+    ss_id << "node_" << n.get();
+    std::string n_id = ss_id.str();
+
+    std::stringstream ss_label;
+    ss_label << std::fixed << std::setprecision(4) << "{ data " << n->data
+             << " | grad " << n->grad << " }";
+    dotFile << "    " << n_id << " [label=\"" << ss_label.str()
+            << "\", shape=record];\n";
+
+    if (!n->_op.empty()) {
+      std::string op_id = n_id + "_op";
+      dotFile << "    " << op_id << " [label=\"" << n->_op << "\"];\n";
+      dotFile << "    " << op_id << " -> " << n_id << ";\n";
+    }
+  }
+  for (const auto& edge : edges) {
+    std::stringstream ss_n1, ss_n2;
+    ss_n1 << "node_" << edge.first.get();
+    ss_n2 << "node_" << edge.second.get();
+
+    dotFile << "    " << ss_n1.str() << " -> " << ss_n2.str() << "_op;\n";
+  }
+
+  dotFile << "}\n";
+  dotFile.close();
+
+  std::string command = "dot -Tsvg graph.dot -o graph.svg";
+  std::system(command.c_str());
+}
